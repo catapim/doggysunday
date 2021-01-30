c98e319684daa0f1e2775f28c4e4b5d05a6130da
phaser <phaser@tubatuba.net>
2021-01-30 18:52:00 -0300

    render

---
+++ js/render.js
@@ -0,0 +1,46 @@
+function render() {
+   
+    
+    // iterate over bodies and fixtures
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    for (let body = world.getBodyList(); body; body = body.getNext()) {
+        // for (var fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
+        //     // draw or update fixture
+        // }
+     
+        let p = body.getPosition();
+        if (body.getUserData() == "fence") {
+            ctx.fillStyle = "#000000";
+            let shape=body.getFixtureList().getShape();
+
+
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.x+shape.getVertex(0).x, p.y+shape.getVertex(0).y);
+            ctx.lineTo(p.x+shape.getVertex(1).x, p.y+shape.getVertex(1).y);
+            ctx.lineTo(p.x+shape.getVertex(2).x, p.y+shape.getVertex(2).y);
+            ctx.lineTo(p.x+shape.getVertex(3).x, p.y+shape.getVertex(3).y);
+
+            ctx.stroke();
+        } else if (body.getUserData() == "sheep") {
+            ctx.fillStyle = "#00FF00";
+            ctx.fillRect(p.x-8, p.y-8, 16, 16);
+
+
+        } else {
+           
+            ctx.fillStyle = "#FF0000";
+
+       
+            ctx.fillRect(p.x-8, p.y-8, 16, 16);
+
+        }
+        
+
+    }
+
+    // request a new frame
+    window.requestAnimationFrame(render);
+
+}
+window.requestAnimationFrame(render);
