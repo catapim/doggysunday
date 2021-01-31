2693649b41d3032b06c9bddaa89b5771c823824f
catalinakranio <cpimentel.kranio@bicevida.cl>
2021-01-30 21:16:36 -0300

    no dogo move

--- js/render.js
+++ js/render.js
@@ -1,104 +1,108 @@
 var dog_sprite = new Image();
-dog_sprite.src="./src/img/dog.gif"
+dog_sprite.src = "./src/img/dog.gif";
 var bg = new Image();
-bg.src="./src/img/grass0.gif" 
+bg.src = "./src/img/grass0.gif";
 
-var bodylist=null;
+var bodylist = null;
 function render() {
-    
-    // iterate over bodies and fixtures
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    // BACKGROUND
-    var pattern = ctx.createPattern(bg, 'repeat');
-    ctx.fillStyle = pattern
-    ctx.fillRect(0,0, WIDTH, HEIGHT)
+  // iterate over bodies and fixtures
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  // BACKGROUND
+  var pattern = ctx.createPattern(bg, "repeat");
+  ctx.fillStyle = pattern;
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
+  // // DOG
 
+  let numColumns = 7;
+  let numRows = 1;
+  // // Define the size of a frame
+  let frameWidth = dog_sprite.width / numColumns;
+  let frameHeight = dog_sprite.height / numRows;
 
-    // // DOG
+  let currentFrame = 0;
 
-    // let numColumns = 7;
-    // let numRows = 1;
-    // // Define the size of a frame
-    // let frameWidth = dog_sprite.width / numColumns;
-    // let frameHeight = dog_sprite.height / numRows;
+  // setInterval(function()
+  // {
+  // Pick a new frame
+  currentFrame++;
+  // Make the frames loop
+  let maxFrame = numColumns * numRows - 1;
+  if (currentFrame > maxFrame) {
+    currentFrame = 0;
+  }
 
-    // let currentFrame = 0
+  // Update rows and columns
+  let column = currentFrame % numColumns;
+  let row = Math.floor(currentFrame / numColumns);
 
-    // setInterval(function()
-    // {
-    //     // Pick a new frame
-    //     currentFrame++;    
-    //     // Make the frames loop
-    //     let maxFrame = numColumns * numRows - 1;
-    //     if (currentFrame > maxFrame){
-    //         currentFrame = 0;
-    //     }
-    
-    //     // Update rows and columns
-    //     let column = currentFrame % numColumns;
-    //     let row = Math.floor(currentFrame / numColumns);
-    
-    //     // Clear and draw
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     ctx.drawImage(dog_sprite, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 10, 30, frameWidth, frameHeight);
-    
-    // //Wait for next step in the loop
-    // }, 1);
-    if(bodylist) {
-        for (let body = bodylist; body; body = body.getNext()) {
-            // for (var fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
-            //     // draw or update fixture
-            // }
-         
-            let p = body.getPosition();
-            let userData=body.getUserData();
-            if (userData == "fence") {
-                ctx.fillStyle = "#000000";
-                let shape=body.getFixtureList().getShape();
-    
-    
-                ctx.moveTo(p.x, p.y);
-                let v=[shape.getVertex(0),shape.getVertex(1),shape.getVertex(2),shape.getVertex(3)]
-                ctx.lineTo(p.x+v[0].x*SCALE, p.y+v[0].y*SCALE);
-                ctx.lineTo(p.x+v[1].x*SCALE, p.y+v[1].y*SCALE);
-                ctx.lineTo(p.x+v[2].x*SCALE, p.y+v[2].y*SCALE);
-                ctx.lineTo(p.x+v[3].x*SCALE, p.y+v[3].y*SCALE);
-    
-                ctx.stroke();
-            } else if (userData == "sheep") {
-                ctx.fillStyle = "#00FF00";
-                let x=p.x*SCALE
-                let y=p.y*SCALE
-                ctx.fillRect(x, y, SCALE*0.1, SCALE*0.1);
-    
-    
-            } else {
-               
-                // ctx.fillStyle = "#FF0000";       
-                // ctx.fillRect(p.x-8, p.y-8, 16, 16);
-                // convert world position to screen positions
-                let x=p.x*SCALE
-                let y=p.y*SCALE
-    
-                ctx.drawImage(dog_sprite,x, y, SCALE*0.1, SCALE*0.1)
-            }
-            
-    
-        }
+  // Clear and draw
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.drawImage(dog_sprite, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 10, 30, frameWidth, frameHeight);
+
+  //Wait for next step in the loop
+  // }, 1);
+  if (bodylist) {
+    for (let body = bodylist; body; body = body.getNext()) {
+      // for (var fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
+      //     // draw or update fixture
+      // }
+
+      let p = body.getPosition();
+      let userData = body.getUserData();
+      if (userData == "fence") {
+        ctx.fillStyle = "#000000";
+        let shape = body.getFixtureList().getShape();
+
+        ctx.moveTo(p.x, p.y);
+        let v = [
+          shape.getVertex(0),
+          shape.getVertex(1),
+          shape.getVertex(2),
+          shape.getVertex(3),
+        ];
+        ctx.lineTo(p.x + v[0].x * SCALE, p.y + v[0].y * SCALE);
+        ctx.lineTo(p.x + v[1].x * SCALE, p.y + v[1].y * SCALE);
+        ctx.lineTo(p.x + v[2].x * SCALE, p.y + v[2].y * SCALE);
+        ctx.lineTo(p.x + v[3].x * SCALE, p.y + v[3].y * SCALE);
+
+        ctx.stroke();
+      } else if (userData == "sheep") {
+        ctx.fillStyle = "#00FF00";
+        let x = p.x * SCALE;
+        let y = p.y * SCALE;
+        ctx.fillRect(x, y, SCALE * 0.1, SCALE * 0.1);
+      } else {
+        // ctx.fillStyle = "#FF0000";
+        // ctx.fillRect(p.x-8, p.y-8, 16, 16);
+        // convert world position to screen positions
+        let x = p.x * SCALE;
+        let y = p.y * SCALE;
+
+        ctx.drawImage(dog_sprite,x, y, SCALE*0.1, SCALE*0.1)
+        ctx.drawImage(
+          dog_sprite,
+          column * frameWidth,
+          row * frameHeight,
+          frameWidth,
+          frameHeight,
+          x,
+          y,
+          frameWidth,
+          frameHeight
+        );
+      }
     }
-    
+  }
 
-    // request a new frame
-    window.requestAnimationFrame(render);
-
+  // request a new frame
+  window.requestAnimationFrame(render);
 }
 window.requestAnimationFrame(render);
-setInterval(function(){
-    world.step(1/60,1,1);
-    bodylist=world.getBodyList();
-    // dog.dogBody.setLinearVelocity(Vec2(x_axis, y_axis));
-    this.world.clearForces();
-        dog.dogBody.applyForce(Vec2(x_axis,y_axis),dog.dogBody.getPosition())
-
-},20)
+setInterval(function () {
+  world.step(1 / 60, 1, 1);
+  bodylist = world.getBodyList();
+  // dog.dogBody.setLinearVelocity(Vec2(x_axis, y_axis));
+  this.world.clearForces();
+  dog.dogBody.applyForce(Vec2(x_axis, y_axis), dog.dogBody.getPosition());
+}, 20);
